7934487cab14d99566bd06b05669dc449be845a0
Nicolás Medero <nicolasmederoc@gmail.com>
2020-05-16 22:31:55 -0300

    primeras funciones de firebase

--- app/src/main/java/utn/frba/mobile/konzern/login/LoginActivity.kt
+++ app/src/main/java/utn/frba/mobile/konzern/login/LoginActivity.kt
@@ -1,6 +1,7 @@
 package utn.frba.mobile.konzern.login
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import utn.frba.mobile.konzern.R
 
@@ -15,5 +16,9 @@ class LoginActivity : AppCompatActivity(), LoginFragment.LoginFragmenView {
                 .replace(R.id.vActivityBaseContent, LoginFragment.newInstance())
                 .commitNow()
         }
+    }
+
+    override fun showSuccessSignInToast() {
+        Toast.makeText(this, "Sign In Successful", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/utn/frba/mobile/konzern/login/LoginFragment.kt
+++ app/src/main/java/utn/frba/mobile/konzern/login/LoginFragment.kt
@@ -1,17 +1,30 @@
 package utn.frba.mobile.konzern.login
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.login_fragment_layout.*
 import utn.frba.mobile.konzern.R
 import java.lang.RuntimeException
 
 class LoginFragment : Fragment() {
 
     private var loginView: LoginFragmenView? = null
+    private lateinit var googleSignInClient : GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -22,6 +35,19 @@ class LoginFragment : Fragment() {
         }
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val googleSignInOptions =GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.firebase_key))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), googleSignInOptions)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        vLoginGoogleSignInButton.setOnClickListener { signIn() }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,9 +56,46 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.login_fragment_layout, container, false)
     }
 
-    companion object {
-        fun newInstance() = LoginFragment()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RC_SIGN_IN) {
+            val task =GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+                loginView?.showSuccessSignInToast()
+                Log.d("GoogleSignIn", "Success, ID: ${account?.idToken}")
+            } catch (e: ApiException) {
+
+            }
+        }
     }
 
-    interface LoginFragmenView
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun firebaseAuthProcess(account: GoogleSignInAccount) {
+        val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
+
+        firebaseAuth.signInWithCredential(credentials)
+            .addOnCompleteListener(requireActivity()){ task ->
+                if (task.isSuccessful) {
+                    Log.d("GoogleSignIn", "Success Firebase Register, ID: ${account.idToken}")
+                } else {
+                    Log.d("GoogleSignIn", "Failed Firebase Register, ID: ${account.idToken}")
+                }
+            }
+    }
+
+    companion object {
+        fun newInstance() = LoginFragment()
+        private const val RC_SIGN_IN = 9001
+    }
+
+    interface LoginFragmenView {
+        fun showSuccessSignInToast()
+    }
 }
