4ed2d3543189e324a828f3cbf3a558b3c189b823
PabloViviani <pablo.viviani@oracle.com>
2020-05-17 21:51:53 -0300

    navegacion inicial a Expensas

--- app/src/main/java/utn/frba/mobile/konzern/MainActivity.kt
+++ app/src/main/java/utn/frba/mobile/konzern/MainActivity.kt
@@ -2,11 +2,26 @@ package utn.frba.mobile.konzern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_main.*
+import utn.frba.mobile.konzern.expenses.ExpensesActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setButtonsListeners()
+    }
+
+    private fun setButtonsListeners(){
+        btn_home_expenses.setOnClickListener{
+            onGotToPosts()
+        }
+    }
+
+    private fun onGotToPosts(){
+        val intent = Intent(this, ExpensesActivity::class.java)
+        this.startActivity(intent)
     }
 }
