35764c031b965c908490dbffc66a6b1342a198af
Nicolás Medero <nicolasmederoc@gmail.com>
2020-05-25 03:39:48 -0300

    Hago unos arreglos en el layout, agrego mock data para visualizar

--- app/src/main/java/utn/frba/mobile/konzern/contact/ContactFragment.kt
+++ app/src/main/java/utn/frba/mobile/konzern/contact/ContactFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.contact_fragment_layout.*
 import utn.frba.mobile.konzern.R
 import java.lang.RuntimeException
 
@@ -30,6 +31,26 @@ class ContactFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setMockInfo()
+    }
+
+    //TODO: Remove this mocked info when get real from firebase
+    private fun setMockInfo() {
+        vContactName.text = "CONSORCIO_ABC"
+        vContactPhone.text = "11-2345-6789"
+        vContactAttentionTime.text = "L a V de 08:00 a 17:00"
+        vContactEmail.text = "consorcio_abc@gmail.com"
+        vContactAddress.text = "Av. Siempreviva 742"
+        vContactExtraInfo.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Nunc nisl est, facilisis sagittis luctus eget, cursus sed quam. In tempus sed ligula at sagittis. " +
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum turpis ipsum, vestibulum non velit et, " +
+                "porta vestibulum erat. Quisque laoreet elit eget nulla ultricies, sed euismod diam tempor. " +
+                "Duis vitae finibus ipsum. Nam accumsan elit a blandit efficitur. Proin dictum pellentesque commodo." +
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Nunc nisl est, facilisis sagittis luctus eget, cursus sed quam. In tempus sed ligula at sagittis. " +
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum turpis ipsum, vestibulum non velit et, " +
+                "porta vestibulum erat. Quisque laoreet elit eget nulla ultricies, sed euismod diam tempor. " +
+                "Duis vitae finibus ipsum. Nam accumsan elit a blandit efficitur. Proin dictum pellentesque commodo."
     }
 
     companion object {
