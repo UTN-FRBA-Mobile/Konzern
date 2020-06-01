e856f153e2c65775f7180bec5a7a4cdf56a2db20
Nicolás Medero <nicolasmederoc@gmail.com>
2020-06-01 20:24:32 -0300

    Agrego xml básico y creo la clase custom

---
+++ app/src/main/java/utn/frba/mobile/konzern/customviews/CustomToolbar.kt
@@ -0,0 +1,12 @@
+package utn.frba.mobile.konzern.customviews
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class CustomToolbar(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+
+    init {
+
+    }
+}
