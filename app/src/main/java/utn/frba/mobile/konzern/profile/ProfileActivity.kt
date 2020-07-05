26b4922125c61db09d184416fa9c4e8928981afb
PabloViviani <pablo.viviani@oracle.com>
2020-07-05 16:57:51 -0300

    agrego progress bar a profile

--- app/src/main/java/utn/frba/mobile/konzern/profile/ProfileActivity.kt
+++ app/src/main/java/utn/frba/mobile/konzern/profile/ProfileActivity.kt
@@ -3,19 +3,27 @@ package utn.frba.mobile.konzern.profile
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_profile.*
 import utn.frba.mobile.konzern.R
 import utn.frba.mobile.konzern.contact.ContactActivity
 import utn.frba.mobile.konzern.customviews.ToolbarMenuInterface
 import utn.frba.mobile.konzern.expenses.ExpensesActivity
+import utn.frba.mobile.konzern.profile.ui.profile.ProfileFragment
 import utn.frba.mobile.konzern.reservations.ReservationsActivity
 
-class ProfileActivity : AppCompatActivity(), ToolbarMenuInterface {
+class ProfileActivity : AppCompatActivity(), ProfileFragment.ProfileFragmentView, ToolbarMenuInterface {
+
+    private lateinit var profileFragment: ProfileFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         vActivityProfileToolbar.setTitle(getString(R.string.profile_toolbar_title))
+        showProgress()
+        if(savedInstanceState == null) {
+            profileFragment = ProfileFragment.newInstance()
+        }
     }
 
     override fun onMenuMyProfileClicked() {}
@@ -40,6 +48,16 @@ class ProfileActivity : AppCompatActivity(), ToolbarMenuInterface {
         val intent = Intent(this, ContactActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    override fun showProgress(){
+        vActivityContentProfile.visibility = View.GONE
+        vProfileProgressBarLayout.visibility = View.VISIBLE
+    }
+
+    override fun hideProgress(){
+        vProfileProgressBarLayout.visibility = View.GONE
+        vActivityContentProfile.visibility = View.VISIBLE
     }
 
 }
--- app/src/main/java/utn/frba/mobile/konzern/profile/ui/profile/ProfileFragment.kt
+++ app/src/main/java/utn/frba/mobile/konzern/profile/ui/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package utn.frba.mobile.konzern.profile.ui.profile
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,6 +12,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -24,6 +26,16 @@ import utn.frba.mobile.konzern.profile.ProfileRepository
 class ProfileFragment : Fragment() {
 
     private val repository = ProfileRepository()
+    private var profileView: ProfileFragmentView? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is ProfileFragmentView) {
+            profileView = context
+        } else {
+            throw RuntimeException("$context must be ProfileFragmentView")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,7 +60,21 @@ class ProfileFragment : Fragment() {
                     .load(lastSignIn?.photoUrl)
                     .error(R.drawable.profile_placeholder)
                     .into(vProfilePicture)
+
+                profileView?.hideProgress()
             }
         })
+    }
+
+    companion object {
+        fun newInstance() =
+            ProfileFragment()
+    }
+
+    interface ProfileFragmentView {
+
+        fun showProgress()
+
+        fun hideProgress()
     }
 }
